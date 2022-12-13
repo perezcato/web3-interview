abb22e8a221453d44b19e5e7ebd1ae91c2ca2bf9
perezcato <perezcatoc@gmail.com>
2022-12-13 12:59:33 +0100

    1. Dashboard component refactoring

--- ui/src/components/Dashboard.tsx
+++ ui/src/components/Dashboard.tsx
@@ -194,6 +194,33 @@ const Roles = (props: Props) => {
             { rolesCount > 0 && rolesOnBlock.map((role, id) => <Role key={id} role={role.role} />) }
           </div>
         </div>
+
+        <div className="w-3/4 border border-gray-800 rounded-lg overflow-hidden h-fit min-h-[400px] flex flex-col">
+          <div className="flex justify-between px-3 py-4 bg-gray-800 items-center">
+            <div className="text-xs">Members</div>
+            <div className="space-x-3">
+              <Button onClick={() => setAddMemberModal(true)} label={"Add Member"} />
+            </div>
+          </div>
+
+          <div className="flex-1 p-3 flex flex-col items-start justify-start wrap space-y-3">
+
+            {
+              membersCount > 0 && members.length > 0 && members.map((member, key) => {
+                return (
+                  <div key={key} className="bg-gray-800 w-full text-xs py-3 px-4 rounded-md flex justify-between">
+                    <div>
+                      {member.address}
+                    </div>
+                    <div className="uppercase text-orange-400">
+                      {member.roleType}
+                    </div>
+                  </div>
+                )
+              })
+            }
+          </div>
+        </div>
       </div>
     </>
 
