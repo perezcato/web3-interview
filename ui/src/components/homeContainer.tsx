b462d8b990342d879922d91e3f3207e7c8a122e3
perezcato <perezcatoc@gmail.com>
2022-12-13 19:56:07 +0100

    persist web3 connection

--- ui/src/components/homeContainer.tsx
+++ ui/src/components/homeContainer.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Login from "./login";
 import {ethers} from "ethers";
 import Dashboard from "./Dashboard";
@@ -8,6 +8,23 @@ const HomeContainer = () => {
   const [provider, _] = React.useState<ethers.providers.Web3Provider>(new ethers.providers.Web3Provider(window.ethereum))
   const [account, setAccount] = React.useState<string>()
   const [signer, setSigner] = React.useState<ethers.Signer>()
+
+
+  useEffect(() => {
+
+    (async () => {
+      try{
+        const accounts: string[] = await window.ethereum.request({ method: 'eth_requestAccounts' })
+        if(accounts && accounts.length > 0){
+          setSigner(provider.getSigner())
+          setAccount(accounts[0])
+        }
+      } catch (e){
+        console.error(e)
+      }
+    })()
+
+  }, [])
 
   return (
     <div className="bg-gray-900 w-screen h-screen text-white">
